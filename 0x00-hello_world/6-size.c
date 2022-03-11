0621a0917e5651b6d1f8ec9c5fc605ba9729ba9d
Tolulope <tolulopeidowu3@gmail.com>
2022-03-11 14:53:56 -0800

    Commit for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- * Return: Always 0 (Success)
+ * main - prints the size of various types
+ * Return: 0 if exit properly, non-zero otherwise
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
+© 2022 GitHub, Inc.
+Term
